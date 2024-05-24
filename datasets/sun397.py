05c48a1727c1e09fa66cfd47725c66ef50378ab9
Theborna <borna710@gmail.com>
2024-05-24 18:52:02 +0330

    modifying SUN397

--- datasets/sun397.py
+++ datasets/sun397.py
@@ -1,4 +1,3 @@
-
 import json
 from pathlib import Path
 from typing import Any, Callable, Optional, Tuple
@@ -6,6 +5,7 @@ from typing import Any, Callable, Optional, Tuple
 import PIL.Image
 from torchvision.datasets.utils import download_and_extract_archive
 from torchvision.datasets.vision import VisionDataset
+from tqdm import tqdm
 
 from datasets.configs.sun397_config import templates
 from datasets.utils.make_dataset_train import make_image_text
@@ -25,6 +25,8 @@ class SUN397(VisionDataset):
         download (bool, optional): If true, downloads the dataset from the internet and
             puts it in root directory. If dataset is already downloaded, it is not
             downloaded again.
+        split (str, optional): Specifies the split to use, either 'train' or 'test'.
+        num_typographic_images (int, optional): Number of typographic images to generate.
     """
 
     _DATASET_URL = "http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz"
@@ -36,13 +38,16 @@ class SUN397(VisionDataset):
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
         download: bool = False,
-        split: str = 'train'
+        split: str = 'train',
+        num_typographic_images: int = 2
     ) -> None:
         super().__init__(root, transform=transform, target_transform=target_transform)
 
         self._data_dir = Path(self.root) / "sun" / "images"
         self._typographic_dir = Path(self.root) / "sun" / "typographic_images"
         self._split = Path(self.root) / "configs" / "split_zhou_SUN397.json"
+        self.num_typographic_images = num_typographic_images
+        self._typographic_image_classes = []
 
         if download:
             self._download()
@@ -61,33 +66,39 @@ class SUN397(VisionDataset):
 
         with open(self._split) as f:
             split_dict = json.load(f)
-        self._all_image_files = [v[0] for v in split_dict["train"]] + [v[0] for v in split_dict["test"]]
-        self._all_labels = [v[1] for v in split_dict["train"]] + [v[1] for v in split_dict["test"]]
-        
+
         if split == 'train':
-            self._split_image_files = [v[0] for v in split_dict["train"]]
-            self._split_labels = [v[1] for v in split_dict["train"]]
+            split_data = split_dict["train"]
         else:
-            self._split_image_files = [v[0] for v in split_dict["test"]]
-            self._split_labels = [v[1] for v in split_dict["test"]]
+            split_data = split_dict["test"]
+
+        self._all_image_files = [v[0] for v in split_data]
+        self._all_labels = [v[1] for v in split_data]
+
+        self._split_image_files = [v[0] for v in split_data]
+        self._split_labels = [v[1] for v in split_data]
 
         self._make_typographic_attack_dataset()
 
     def __len__(self) -> int:
         return len(self._split_image_files)
 
-    def __getitem__(self, idx) -> Tuple[Any, Any, Any]:
-        image_file, typographic_image_file, label = self._data_dir / self._split_image_files[idx], self._typographic_dir / self._split_image_files[idx], self._split_labels[idx]
-        image, typographic_image = PIL.Image.open(image_file).convert("RGB"), PIL.Image.open(typographic_image_file).convert("RGB")
+    def __getitem__(self, idx):
+        image_file = self._image_files[idx]
+        typographic_image_files = self._typographic_image_files[idx]
+        label = self._labels[idx]
+        typographic_label = self._typographic_image_classes[idx]
+        image = PIL.Image.open(image_file).convert("RGB")
+        typographic_images = [PIL.Image.open(typographic_image_file).convert("RGB") for typographic_image_file in typographic_image_files]
 
         if self.transform:
             image = self.transform(image)
-            typographic_image = self.transform(typographic_image)
+            typographic_images = [self.transform(typographic_image) for typographic_image in typographic_images]
 
         if self.target_transform:
-            label = self.target_transform(label)
+            label = self.target_transform(label)    
 
-        return image, typographic_image ,label
+        return image, typographic_images, label, typographic_label
 
     def _check_exists(self) -> bool:
         return self._data_dir.is_dir()
@@ -103,7 +114,9 @@ class SUN397(VisionDataset):
     def _make_typographic_attack_dataset(self) -> None:
         if self._check_exists_synthesized_dataset():
             return
-        for i, file in enumerate(self._split_image_files):
-            print(file)
-            make_image_text(file, self.classes, self._data_dir, self._typographic_dir, self._split_labels[i])
-            
+        for i, file in tqdm(enumerate(self._split_image_files), total=len(self._split_image_files)):
+            labels = make_image_text(
+                file, self.classes, self._data_dir, self._typographic_dir, self._split_labels[i], 
+                num_typographic_images=self.num_typographic_images
+            )
+            self._typographic_image_classes.append(labels)
